b799e8f6276a5bbd3a97bf8df0edf3650639353c
xf0r3m <morketsmerke@gmail.com>
2021-09-24 11:53:18 +0200

    Dodanie kategorii artykułów

---
+++ articles/linux/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="icon" type="image/png" href="https://files.morketsmerke.net/resources/mm.png">
+		<style>
+			.code-block {
+				display: block;
+				background-color: silver;
+				font-family: monospace;
+				font-weight: bolder;
+			}
+			.code-inline {
+				background-color: silver;
+				font-family: monospace;
+
+				font-weight: bolder;
+			}
+			p { text-align: justify; }
+		</style>
+	</head>
+	<body style="font-family: monospace;" >
+<pre>
+  _____ _   _ _    _    ___      _
+ / ____| \ | | |  | |  / / |    (_)
+| |  __|  \| | |  | | / /| |     _ _ __  _   ___  __
+| | |_ | . ` | |  | |/ / | |    | | '_ \| | | \ \/ /
+| |__| | |\  | |__| / /  | |____| | | | | |_| |>  <
+ \_____|_| \_|\____/_/   |______|_|_| |_|\__,_/_/\_\
+</pre>
+	<p style="margin: 15px; padding: 0; outline: 0; font-size: 18pt;">
+		&#9760;&nbsp;<a href="https://morketsmerke.github.io">morketsmerke</a>&nbsp;&#9760;
+	</p>
+	<div style="margin-left: auto; margin-right: auto; width: 80%;">
+		<ul style="list-style-type: none;">
+      <li><a href="Alpine_linux_jako_bramka_sieciowa.html">
+        Alpine Linux jako bramka sieciowa</a></li>
+      <li><a href="generowanie_certyfikatów_self-signed_OpenSSL.html">
+        Generowanie certyfikatów self-signed OpenSSL</a></li>
+      <li><a href="generowanie_certyfikatu_Let's_Encrypt_typu_wildcard.html">Generowanie certyfikatu Let's Encrypt typu wildcard</a></li>
+      <li><a href="instalacja_aktualizacja_pakietów_na_Arch_Linux.html">Instalacja i aktualizacja pakietów na Arch Linux</a></li>
+      <li><a href="instalacja_Alpine_linux.html">Instalacja Alpine Linux</a></li>
+      <li><a href="instalacja_kontenerów_LXD.html">Instalacja kontenerów LXD</a></li>
+      <li><a href="instalacja_sandstorm.io.html">Instalacja Sandstorm.io</a></li>
+      <li><a href="instalacja_Spotify_oraz_atom.io_za_pomocą_snap.html">Instalacja Spotufy oraz atom.io za pomocą Snap</a></li>
+      <li><a href="instalacja_systemu_z_własnego_obrazu_LiveCD z Debianem.html">Instalacja systemu z własnego obrazu LiveCD</a></li>
+      <li><a href="NFS_-_udostepnianie_i_montowanie_udziałów.html">NFS - udostępnienianie i montowanie udziałów</a></li>
+      <li><a href="przekierowanie_portów_do_kontenerów_LXD.html">Przekierowanie portów do kontenerów LXD</a></li>
+      <li><a href="samba_AD_DC_-_Instalacja.html">Samba AD DC - Instalacja</a></li>
+      <li><a href="samba_AD_DC_-_NTP.html">Samba AD DC - NTP</a></li>
+      <li><a href="tworzenie_logicznych_woluminów_LVM.html">Tworzenie Logicznych woluminów LVM.</a></li>
+      <li><a href="tworzenie_RAID_programowych_w_systemach_GNU_Linux.html">Tworzenie RAID programowych na systemach GNU Linux</a></li>
+      <li><a href="własne_LiveCD_z_Debianem.html">Własne LiveCD z Debianem</a></li>
+		</ul>
+	</div>
+
+	<p style="margin: 15px; padding: 0; outline: 0;">
+		2021; COPYLEFT; ALL RIGHT REVERSED;
+	</p>
+
+	</body>
+</html>
